83418c731af8f659310e40a32995d7f890f31720
Curtisaurus <ccdrullinger@gmail.com>
2021-06-28 23:17:31 -0400

    created product read and delete routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,49 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  // including their associated Category and Tag data
+  try {
+    const productData = Product.findAll({
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          as: 'tags'
+        }
+      ]
+    });
+
+    res.status(200).jsoon(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // including its associated Category and Tag data
+  try {
+    const productData = Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          as: 'tags'
+        }
+      ]
+    });
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +127,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
